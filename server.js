ac08e86f102428e7f412b637b2b7f641c8a568aa
Matt <boblikesbbq@gmail.com>
2017-07-23 12:35:28 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const moment = require("moment");
+const hbs = require('hbs');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+app.get('/api/:time', (req, res) => {
+  var time = req.params.time;
+  var natural = moment(time, ['MM-DD-YYYY', 'MMMM-DD-YYYY']).format("MMMM  DD YYYY");
+  var unix = moment(time, 'MMMM-DD-YYYY').format("X");
+
+  res.json({unix: parseInt(unix), natural: natural});
+});
+
+app.listen(port, ()=>{
+  console.log(`Listening to Port ${port}`);
+})
